2bbc4e8198e1a49b0c0bbbdd09ae9a03dc43e949
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-16 01:01:09 -0500

    add note to db with new routes logic

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
+
 // initialize express app 
 const app = express();
+
 // set server port 3001
 const PORT = process.env.PORT || 3001;
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
 // listen on port 3001
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT " + PORT + "!");
 }); 
